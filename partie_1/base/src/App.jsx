a334d34353f06033ce68996431c6f4733e507807
scotty <scottyndanga02400@gmail.com>
2026-01-25 12:08:36 +0100

    exercice finish

--- partie_1/base/src/App.jsx
+++ partie_1/base/src/App.jsx
@@ -1,21 +1,87 @@
-import { useState } from 'react';
-import { useContext } from "react";
-import { ThemeContext } from "./context/ThemeContext";
-import CounterDisplay from './Components/CounterDisplay';
-import CounterButton from './Components/CounterButtons';
+import { useRef, useEffect } from "react";
+import { useCallback, useState, useMemo } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const { theme, toggleTheme } = useContext(ThemeContext);
+function FocusInput() {
+  const inputRef = useRef(null);
 
   return (
-    <div className={theme}>
-      <button onClick={toggleTheme}>Switch Theme</button>
-      <h1>Shared Counter</h1>
-      <CounterDisplay count={count} />
-      <CounterButton setCount={setCount} />
+    <div>
+      <input ref={inputRef} />
+      <button onClick={() => inputRef.current.focus()}>
+        focus
+      </button>
+    </div>
+  )
+}
+
+function RenderCounter() {
+  const renderCounter = useRef(0);
+
+  useEffect(() => {
+    renderCounter.current += 1;
+  });
+
+  return <h2>Renders: {renderCounter.current}</h2>;
+}
+
+
+function Child({ onClick }) {
+  console.log("child render");
+  return <button onClick={onClick}>Click child</button>
+}
+
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    console.log('child clicked');
+  }, []);
+
+  console.log("Parent render");
+  
+  return (
+    <div>
+      <h2>Parent count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <Child onClick={handleClick}/>
     </div>
   );
+}
+
+const products = [
+  { id: 1, name: "Keyboard", price: 50 },
+  { id: 2, name: "Mouse", price: 30 },
+  { id: 3, name: "Screen", price: 200 }
+];
+
+function ProductList() {
+  const [count, setCount] = useState(0);
+
+  const totalPrice = useMemo(() => {
+    console.log("Calcul du total");
+    return products.reduce((acc, p) => acc + p.price, 0);
+  }, []);
+
+  return (
+    <div>
+      <h2>Total: {totalPrice} €</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Re-render ({count})
+      </button>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <FocusInput/>
+      <RenderCounter/>
+      <Parent/>
+      <ProductList/>
+    </div>
+  )
 }
 
 export default App;
